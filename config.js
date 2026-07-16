56edf10d150cb873ca0cc2d4c895ed4f90558e95
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-16 13:39:59 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 /**
  * DPRO 中古車買取・販売 LINE
- * STEP CAR-5 共通設定
+ * STEP CAR-6 共通設定
  */
 window.DPRO_CAR_CONFIG = Object.freeze({
-  version: "CAR-5-INVENTORY-DETAIL-TEST-DRIVE-20260716",
+  version: "CAR-6-LINE-MEMBER-PAGE-20260716",
   apiBase: "https://dpro-used-car-line-api.dpromstk2000.workers.dev",
   companyCode: "dpro_used_car_demo",
+  liffId: "",
   defaultLineUserId: "demo_car_line_001",
   reservationDaysAhead: 60,
   slotMinutes: 30,
